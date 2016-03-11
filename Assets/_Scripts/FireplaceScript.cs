07a8f7b47f252515dff5242e1a651db443dfd7b7
MaciejSzpakowski <systemzajety@yahoo.com>
2016-03-10 23:13:08 -0600

    fireplace

---
+++ Assets/_Scripts/FireplaceScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireplaceScript : MonoBehaviour
+{
+    GameObject light;
+	// Use this for initialization
+	void Start ()
+    {
+        light = transform.FindChild("firelight").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        light.GetComponent<Light>().intensity = Mathf.Sin(Time.realtimeSinceStartup * 10) / 10 + 1;
+    }
+}
