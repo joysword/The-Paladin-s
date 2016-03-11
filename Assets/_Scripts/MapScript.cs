5137f412a0821e89f047a80e92451db13f153fcf
MaciejSzpakowski <systemzajety@yahoo.com>
2016-03-10 20:52:35 -0600

    map script fix

--- Assets/_Scripts/MapScript.cs
+++ Assets/_Scripts/MapScript.cs
@@ -5,11 +5,9 @@ public class MapScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start ()
-    {
-        var map = GameObject.Find("map");
-        
+    {        
         // add mesh colliders
-        foreach(Transform c in map.transform)
+        foreach(Transform c in transform)
         {
             c.gameObject.AddComponent<MeshCollider>();
         }	
