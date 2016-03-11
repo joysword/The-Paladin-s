6917671e7c7393add1b41b0c62b3f7cec7d58bc8
Shi Yin <joysword@me.com>
2016-03-11 04:46:46 -0600

    now we can press X button to reset the scene

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using getReal3D;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (getReal3D.Input.GetButtonDown("ChangeWand")) {
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
