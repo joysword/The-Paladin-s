cec2ff304449a6c47cc2bbbd142d0ef73b1ead6d
Kamil Piekutowski <kpieku2@uic.edu>
2016-03-17 22:30:14 -0700

    trap door trigerred when woodenlever highlighted

--- Assets/_Scripts/TrapLever.cs
+++ Assets/_Scripts/TrapLever.cs
@@ -4,6 +4,11 @@ using System.Collections;
 public class TrapLever : MonoBehaviour {
 
 	public bool isActive;
+	public bool trapDown = false;
+	public Material Glow;
+	public Material Normal;
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -12,13 +17,35 @@ public class TrapLever : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (isActive) {
-			Debug.Log ("LEVER ACTIVE");
+		if (Input.GetKeyDown("e")){
+			if (isActive) {
+				Debug.Log ("LEVER ACTIVE");
+				var trap = GameObject.FindGameObjectWithTag("TrapDoor1");
+				if(!trapDown){
+					trap.transform.Rotate(0,0,90);
+					trapDown = true;
+				}
+				else{
+					trap.transform.Rotate(0,0,-90);
+					trapDown = false;
+				}
+			}
+
 		}
+
 	}
 	void OnTriggerStay(Collider other)
 	{
 		if (other.gameObject.tag == "Player") {
+			Component halo = GetComponent("Halo"); 
+			halo.GetType().GetProperty("enabled").SetValue(halo,true,null);
+			var materials = GetComponentsInChildren<Renderer>();
+			//var materials = GetComponentsInChildren<Renderer>().materials;
+			Debug.Log ("Materail count: " + materials.Length);
+			foreach ( var x in materials){
+				Debug.Log ("Materail name: " + x);
+				x.material.SetColor("_Color",Color.red);
+			}
 
 			isActive = true;
 		}
@@ -26,9 +53,20 @@ public class TrapLever : MonoBehaviour {
 	void OnTriggerExit(Collider other)
 	{
 		if (other.gameObject.tag == "Player") {
-			
+			Component halo = GetComponent("Halo"); 
+			halo.GetType().GetProperty("enabled").SetValue(halo,false,null);
+			var materials = GetComponentsInChildren<Renderer>();
+			//var materials = GetComponentsInChildren<Renderer>().materials;
+			Debug.Log ("Materail count: " + materials.Length);
+			foreach ( var x in materials){
+				Debug.Log ("Materail name: " + x);
+				x.material.SetColor("_Color",Color.white);
+			}
+
 			isActive = false;
 		}
 	}
+
+
 
 }
