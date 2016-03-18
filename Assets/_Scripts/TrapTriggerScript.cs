d73842485fcb83fdf06f40c6d290e7aaa53ad4b9
Kamil Piekutowski <kpieku2@uic.edu>
2016-03-17 22:58:47 -0700

    trapdoor mesh fix

---
+++ Assets/_Scripts/TrapTriggerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapTriggerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.name == "guard"){
+			var trapdoor = GameObject.Find ("trap_door_levr");
+			bool isDown = trapdoor.GetComponent<TrapLever> ().trapDown;
+			if(isDown){
+				GameObject.Destroy(other.gameObject);
+			}
+		}
+
+	}
+}
