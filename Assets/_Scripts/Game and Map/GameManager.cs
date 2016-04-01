1fa237fce3f9739856c04fc67dc4cf6de84f6eb2
Shi Yin <joysword@me.com>
2016-04-01 13:49:36 -0500

    added script to jump between features and levels

--- Assets/_Scripts/Game and Map/GameManager.cs
+++ Assets/_Scripts/Game and Map/GameManager.cs
@@ -3,17 +3,36 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+    public Transform[] spawnPointsPast;
+    public Transform[] spawnPointsFuture;
+
+    public GameObject player1;
+    public GameObject player2;
+
+    int index = 0;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        setPosition();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if (getReal3D.Input.GetButtonDown("ChangeWand")) {
-			ReloadScene();
+		if (Input.GetKeyDown("i") || getReal3D.Input.GetButtonDown("ChangeWand")) {
+            if (index >= spawnPointsPast.Length) {
+                ReloadScene();
+            }
+            setPosition();
 		}
 	}
+
+    void setPosition() {
+        player1.transform.position = spawnPointsPast[index].position;
+        player1.transform.rotation = spawnPointsPast[index].rotation;
+        player2.transform.position = spawnPointsFuture[index].position;
+        player2.transform.rotation = spawnPointsFuture[index].rotation;
+        index++;
+    }
 
 	public void ReloadScene() {
 		Application.LoadLevel(Application.loadedLevel);
