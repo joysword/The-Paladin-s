eab373bf0a0fd51badf2e52437ba6e134a8c1ac9
Shi Yin <joysword@me.com>
2016-04-03 19:27:04 -0500

    fixing bugs

--- Assets/_Scripts/Game and Map/OpeningManager.cs
+++ Assets/_Scripts/Game and Map/OpeningManager.cs
@@ -2,22 +2,30 @@
 
 public class OpeningManager : MonoBehaviour {
 
-    public GameObject text1;
-    public GameObject text2;
-    public GameObject text3;
+    public GameObject text1p;
+    public GameObject text2p;
+    public GameObject text3p;
+	public GameObject text1f;
+	public GameObject text2f;
+	public GameObject text3f;
+
 
     void Update() {
         if (Input.GetKeyDown("f") || getReal3D.Input.GetButtonDown("Reset")) {
             Debug.Log("pressed");
-            if (text1.activeInHierarchy) {
-                text1.SetActive(false);
-                text2.SetActive(true);
+            if (text1p.activeInHierarchy) {
+                text1p.SetActive(false);
+				text1f.SetActive(false);
+				text2p.SetActive(true);
+				text2f.SetActive(true);
             }
-            else if (text2.activeInHierarchy) {
-                text2.SetActive(false);
-                text3.SetActive(true);
+            else if (text2p.activeInHierarchy) {
+                text2p.SetActive(false);
+				text2f.SetActive(false);
+                text3p.SetActive(true);
+				text3f.SetActive(true);
             }
-            else if (text3.activeInHierarchy) {
+            else if (text3p.activeInHierarchy) {
                 Application.LoadLevel(Application.loadedLevel + 1);
             }
         }
