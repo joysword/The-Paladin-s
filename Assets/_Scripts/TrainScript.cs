6c8e41ec8552f194d30385470474c689b084a7d7
MaciejSzpakowski <systemzajety@yahoo.com>
2016-04-07 21:40:20 -0500

    train

---
+++ Assets/_Scripts/TrainScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TrainScript : MonoBehaviour {
+    Vector3 resetPos;
+    float velocity = -35;
+    AudioSource horn;
+	// Use this for initialization
+	void Start () {
+        resetPos = transform.position;
+        horn = new List<AudioSource>(GetComponents<AudioSource>()).Find(t => t.clip.name == "horn");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(velocity * Time.deltaTime, 0, 0);
+
+        if (transform.position.x < -1000)
+            transform.position = resetPos;
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("horn");
+        horn.Play();
+    }
+}
