d9ce45a5dd16eb650c4212af149ec7d1f5652177
Shi Yin <joysword@me.com>
2016-04-21 17:30:27 -0500

    added opening credits; updated opening story and tutorial

--- Assets/_Scripts/Game and Map/OpeningManager.cs
+++ Assets/_Scripts/Game and Map/OpeningManager.cs
@@ -2,18 +2,37 @@
 
 public class OpeningManager : MonoBehaviour {
 
+    public GameObject titlep;
+    public GameObject subtitlep;
+    public GameObject continuep;
+
     public GameObject text1p;
     public GameObject text2p;
     public GameObject text3p;
-	public GameObject text1f;
+
+    public GameObject titlef;
+    public GameObject subtitlef;
+    //public GameObject continuef;
+
+    public GameObject text1f;
 	public GameObject text2f;
 	public GameObject text3f;
 
 
     void Update() {
         if (Input.GetKeyDown("f") || getReal3D.Input.GetButtonDown("Reset")) {
-            Debug.Log("pressed");
-            if (text1p.activeInHierarchy) {
+            if (titlep.activeInHierarchy) {
+                titlep.SetActive(false);
+                subtitlep.SetActive(false);
+                titlef.SetActive(false);
+                subtitlef.SetActive(false);
+                
+                text1p.SetActive(true);
+                continuep.GetComponent<TextMesh>().text = "Press O to continue..";
+                text1f.SetActive(true);
+                //continuef.GetComponent<TextMesh>().text = "Press O to continue..";
+            }
+            else if (text1p.activeInHierarchy) {
                 text1p.SetActive(false);
 				text1f.SetActive(false);
 				text2p.SetActive(true);
@@ -22,7 +41,9 @@ public class OpeningManager : MonoBehaviour {
             else if (text2p.activeInHierarchy) {
                 text2p.SetActive(false);
 				text2f.SetActive(false);
+
                 text3p.SetActive(true);
+                continuep.GetComponent<TextMesh>().text = "Press O to start";
 				text3f.SetActive(true);
             }
             else if (text3p.activeInHierarchy) {
